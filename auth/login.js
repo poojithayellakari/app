af6b1c394892de0ba04f999c796c76f48ada1a65
Vamsidhar <vamsubala1@gmail.com>
2022-11-01 18:38:56 +0530

    Login isssue resolved

--- auth/login.js
+++ auth/login.js
@@ -1,8 +1,8 @@
-var express =require('express');
-var app =express();
+var express = require("express");
+var app = express();
 
-function login(req,res){
-    console.log(req.body);
-    res.send("successfully logged in", +req.body.email_id);
+function login(req, res) {
+  console.log(req.body);
+  res.send("successfully logged in" + req.body.email_id);
 }
-module.exports= login;
+module.exports = login;
